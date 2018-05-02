01ea6e2cd604b7526690873a7f56dc1556152c96
NatalieBlack <natalievictoriablack@gmail.com>
2018-05-01 20:10:52 -0400

    Assign owners to projects in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,19 @@ Reward.destroy_all
 User.destroy_all
 Project.destroy_all
 
+5.times do
+  User.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.free_email,
+    password: 'password',
+    password_confirmation: 'password'
+  )
+end
+
 10.times do
   project = Project.create!(
+              user: User.all.sample,
               title: Faker::App.name,
               description: Faker::Lorem.paragraph,
               goal: rand(100000),
@@ -20,21 +31,17 @@ Project.destroy_all
   end
 end
 
-5.times do
-  User.create!(
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
-    email: Faker::Internet.free_email,
-    password: 'password',
-    password_confirmation: 'password'
-  )
-end
 
 20.times do
   project = Project.all.sample
+  backer = User.all.sample
+
+  while backer == project.user
+    backer = User.all.sample
+  end
 
   Pledge.create!(
-    user: User.all.sample,
+    user: backer,
     project: project,
     dollar_amount: project.rewards.sample.dollar_amount + rand(10)
   )
