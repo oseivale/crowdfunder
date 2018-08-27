15d6b1d4e8eccfb14fcd5560fd2283a3fa546d2c
Valerie Osei <valerie.osei@gmail.com>
2018-08-27 17:30:59 -0400

    edited suer method, and re-tested

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
+  has_many :pledges
+  has_many :projects
 
   validates :password, length: { minimum: 8 }, on: :create
   validates :password, confirmation: true, on: :create
