f9d85503cf99c4d13dff82cbd3b8eac2d351af6c
Valerie Osei <valerie.osei@gmail.com>
2018-08-27 17:14:54 -0400

    modified tests

--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -2,6 +2,33 @@ require 'test_helper'
 
 class ProjectTest < ActiveSupport::TestCase
 
+  def new_user
+    User.destroy_all
+    User.create(
+      first_name:            'Sally',
+      last_name:             'Lowenthal',
+      email:                 'sally@example.com',
+      password:              'passpass',
+      password_confirmation: 'passpass'
+    )
+  end
+
+  def new_project
+    Project.new(
+      title:       'Cool new boardgame',
+      description: 'Trade sheep',
+      start_date:  Date.today + 1.day,
+      end_date:    Date.today + 1.month,
+      goal:        50000,
+      user_id:     new_user.id
+    )
+  end
+
+  def test_project_is_valid
+    project = new_project
+    assert project.valid?
+  end
+
   def test_valid_project_can_be_created
     owner = new_user
     owner.save
@@ -20,48 +47,26 @@ class ProjectTest < ActiveSupport::TestCase
     assert project.invalid?, 'Project should not save without owner.'
   end
 
-  def new_project
-    Project.new(
-      title:       'Cool new boardgame',
-      description: 'Trade sheep',
-      start_date:  Date.today + 1.day,
-      end_date:    Date.today + 1.month,
-      goal:        50000
-    )
-  end
 
-  def new_user
-    User.new(
-      first_name:            'Sally',
-      last_name:             'Lowenthal',
-      email:                 'sally@example.com',
-      password:              'passpass',
-      password_confirmation: 'passpass'
-    )
-  end
+
+
 
   def test_project_start_cannot_be_in_past
     # project1 = Project.new(start_date: Date.today)
     project = new_project
     project.start_date = Date.today - 1.day
-    expect = false
-    actual = project.valid?
-    assert_equal(expect, actual)
+    assert project.invalid?
   end
 
   def test_project_end_date_cannot_be_earlier_than_start_date
     project = new_project
     project.end_date = project.start_date - 1.day
-    expect = false
-    actual = project.valid?
-    assert_equal(expect, actual)
+    assert project.invalid?
   end
 
   def test_project_goal_is_positive_number
     project = new_project
-    project.goal = -100
-    expect = false
-    actual = project.valid?
-    assert_equal(expect, actual)
+    project.goal = 10
+    assert project.invalid?
   end
 end
