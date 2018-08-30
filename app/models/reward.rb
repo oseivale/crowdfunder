57230c51d7960c99b7783035edf96777d67d2cc3
Billy Davidson <billy.davids52@gmail.com>
2018-08-30 14:02:39 -0400

    wip rewards

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -1,8 +1,9 @@
 class Reward < ActiveRecord::Base
   belongs_to :project
+  belongs_to :pledges
 
   validates :description, :dollar_amount, presence: true
   validates :dollar_amount, numericality: {greater_than: 0}
-  
+
 
 end
